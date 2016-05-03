e6c79f730ccda05317592b42cd64fe659f53404f
andhl <dhlwwhl@163.com>
2016-05-03 10:02:16 +0800

    commit test

--- app/src/main/java/com/android/hl/ui/FragmentActivity.java
+++ app/src/main/java/com/android/hl/ui/FragmentActivity.java
@@ -52,7 +52,7 @@ public class FragmentActivity extends BaseActivity {
 
     private void initXG() {
         // 开启logcat输出，方便debug，发布时请关闭
-     XGPushConfig.enableDebug(this, true);
+        XGPushConfig.enableDebug(this, true);
 // 如果需要知道注册是否成功，请使用registerPush(getApplicationContext(), XGIOperateCallback)带callback版本
 // 如果需要绑定账号，请使用registerPush(getApplicationContext(),account)版本
 // 具体可参考详细的开发指南
